2e7f23e4c694912d42cf2e783a04cb320f64d34c
Cristina F <77860767+cristinafor@users.noreply.github.com>
2023-01-09 14:48:28 +0100

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from "./logo-cri.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Cristina Fornasier and is open-sourced on
+            GitHub
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
