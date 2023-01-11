d8b2e77200648d8480a4a88a737878fedf5581f1
Cristina F <77860767+cristinafor@users.noreply.github.com>
2023-01-11 21:26:08 +0100

    Small changes to css

--- src/App.js
+++ src/App.js
@@ -14,8 +14,22 @@ function App() {
         </main>
         <footer className="App-footer">
           <small>
-            This project was coded by Cristina Fornasier and is open-sourced on
-            GitHub
+            This project was coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/cristinafornasier/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Cristina Fornasier
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a
+              href="https://github.com/cristinafor/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
           </small>
         </footer>
       </div>
